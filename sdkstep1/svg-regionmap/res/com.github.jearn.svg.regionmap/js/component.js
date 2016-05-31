083d7e7e0df9a8b93a6c282fa370c0655ae445c2
Jens Arnold <jearn@users.noreply.github.com>
2016-05-31 21:35:16 +0200

    Created sdkstep1: - setup project svg-regionmap in eclipse - verified to work in SAP Design Studio 16.0.5

---
+++ sdkstep1/svg-regionmap/res/com.github.jearn.svg.regionmap/js/component.js
@@ -0,0 +1,6 @@
+sap.designstudio.sdk.Component.subclass("com.github.jearn.svg.regionmap.SVGRegionMap", function() {
+
+	this.init = function() {
+		this.$().append("<div>SVGRegionMap</div>");
+	};
+});
